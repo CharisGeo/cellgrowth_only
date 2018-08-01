11e47321a55edba1b7910d361fdf9f6c21cc9f4d
CharisGeo <40542077+CharisGeo@users.noreply.github.com>
2018-08-01 10:58:26 +0300

    Update cellgrowth.h

--- src/cellgrowth.h
+++ src/cellgrowth.h
@@ -18,19 +18,33 @@
 
 namespace bdm {
 
+struct GrowthModule : public BaseBiologyModule {
+   GrowthModule() : BaseBiologyModule(gAllBmEvents) {}
+
+template <typename T, typename TSimulation = Simulation<>>
+  void Run(T* cell) {
+  if (cell->GetDiameter() < 60) {
+      cell->ChangeVolume(4000);
+    }
+   }
+    ClassDefNV(GrowthModule, 1);
+   };
+
 // Define compile time parameter
 template <typename Backend>
-struct CompileTimeParam : public DefaultCompileTimeParam<Backend> {};
-
+struct CompileTimeParam : public DefaultCompileTimeParam<Backend> {
+  using BiologyModules = Variant<GrowthModule>;
+};
+ 	
 inline int Simulate(int argc, const char** argv) {
   Simulation<> simulation(argc, argv);
 
   // Define initial model - in this example: single cell at origin
   auto* rm = simulation.GetResourceManager();
   auto&& cell = rm->New<Cell>(30);
+  cell.AddBiologyModule(GrowthModule());
 
-  // Run simulation for one timestep
-  simulation.GetScheduler()->Simulate(1);
+  simulation.GetScheduler()->Simulate(3000);
 
   std::cout << "Simulation completed successfully!" << std::endl;
   return 0;
